9eacca8df0c5a2a892c4cd3e2dedff5a5df20069
Maria Geyzianny de Sousa Silva <geizy@Marias-MacBook-Air.local>
2023-05-30 23:02:02 -0300

    voltando a praticar lógica de programação

---
+++ camadas.js
@@ -0,0 +1,25 @@
+function solucao(number){
+    if (number >= 0 & number <= 20){
+        print("TROPOSFERA")
+
+    } else if (number > 20 & number <= 50 ){
+        print("ESTRATOSFERA")
+
+    } else if (number > 50 & number <= 80){
+        print("MESOSFERA")
+
+    } else if (number > 80 & number <= 450) {
+        print ("TERMOSFERA")
+
+    } else if (number > 450 & number <= 900){
+        print("EXOSFERA")
+    }
+}
+
+solucao(number)
+
+let number  = require('fs').readFileSync('/dev/')
+        
+
+
+
